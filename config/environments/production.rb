394348569b1921669571fe103c1a0bd3e4e0142c
giannijahziel <giannijahziel@gmail.com>
2016-12-21 08:33:23 -0500

    remove assets.precompile prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  config.assets.precompile += %w( *.js *.css )
+  # config.assets.precompile += %w( *.js *.css )
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
